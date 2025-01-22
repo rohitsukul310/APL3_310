e3292da289060860397d598e4c4cce7eb66d2f50
221B310 <221B310@juet.co.in>
2025-01-22 14:33:13 +0530

    adding new file

---
+++ LE_01/question_02/Overriding.java
@@ -0,0 +1,25 @@
+
+class Mother {
+    void show() {
+        System.out.println("Hello World");
+    }
+}
+
+
+class Child extends Mother {
+    @Override
+    void show() {
+        System.out.println("Hello JUET");
+    }
+}
+
+
+public class Overriding {
+    public static void main(String[] args) {
+        Mother obj1 = new Mother();
+        obj1.show(); 
+
+        Child obj2 = new Child();
+        obj2.show(); 
+    }
+}
